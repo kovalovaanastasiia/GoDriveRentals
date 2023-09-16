95563b75e7cc9a8bbc29e58a03c8bb1712f21c0e
kovalovaanastasiia <tokach26@gmail.com>
2023-09-16 22:45:11 +0300

    favorites page has been added

--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import css from "../Catalog/Catalog.module.css";
+import {CarAdvert} from "../../components/CarAdvert/CarAdvert";
 
 export const Favorites = () => {
+  const [favoriteCars, setFavoriteCars] = React.useState(
+    JSON.parse(localStorage.getItem('favoriteCars')) || []
+  );
 
+  const removeFromFavorites = (index) => {
+    const updatedFavoriteCars = [...favoriteCars];
+    updatedFavoriteCars.splice(index, 1);
+
+    localStorage.setItem('favoriteCars', JSON.stringify(updatedFavoriteCars));
+    setFavoriteCars(updatedFavoriteCars);
+  };
   return (
-    <div>
-      <p>Favorites</p>
-    </div>
+    <>
+      <ul className={css.carsList}>
+        {favoriteCars.map(favoriteCar => (
+          <li key={favoriteCar.id}>
+            <CarAdvert car={favoriteCar} favoriteCars={favoriteCars} setFavoriteCars={removeFromFavorites}/>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
